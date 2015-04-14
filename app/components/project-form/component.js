3e2b1946012d4b8c485e15d0b6f843634c6efdad
Han Loong Liauw <hanloongliauw@gmail.com>
2015-04-15 00:00:29 +1000

    fixed minor things

--- app/components/project-form/component.js
+++ app/components/project-form/component.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement: function() {
+    Ember.$('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+  },
+  isDirty: function() {
+    var model = this.get('model');
+    return !model.get('isNew');
+  }.property('model'),
   actions: {
     save: function() {
       this.sendAction('save', this.get('model'));
--- app/projects/edit/route.js
+++ app/projects/edit/route.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('project', params.project_id);
+  },
+  actions: {
+    updateProject: function(editProject) {
+      editProject.save().then((project) => {
+        this.transitionTo('projects');
+      }).catch(function() {
+      });
+    }
+  }
 });
