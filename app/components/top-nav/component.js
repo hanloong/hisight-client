77ee23a9a06382521b0b60ea7451d55d8ddda452
Han Loong Liauw <hanloongliauw@gmail.com>
2015-04-17 00:30:41 +1000

    sidebar hidden by default

--- app/components/top-nav/component.js
+++ app/components/top-nav/component.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement: function() {
-    Ember.$(".sidebar-toggle").sideNav();
+    Ember.$(".sidebar-toggle").sideNav({closeOnClick: true});
   }
 });
