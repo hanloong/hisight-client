0ff064d65a638c95d19849bd1be32c89dd86b3c4
Han Loong Liauw <hanloongliauw@gmail.com>
2015-04-17 22:45:22 +1000

    more style cleanups

--- app/components/top-nav/component.js
+++ app/components/top-nav/component.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement: function() {
     Ember.$(".sidebar-toggle").sideNav({closeOnClick: true});
+    Ember.$('.modal-trigger').leanModal();
   }
 });
--- tests/unit/components/forms/project-edit/component-test.js
+++ tests/unit/components/forms/project-edit/component-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForComponent('project-form', {
+moduleForComponent('forms/project-edit', {
   // Specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
 });
