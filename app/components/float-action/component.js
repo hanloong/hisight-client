e1444368d2ba318baf508957d897d452c9997193
Han Loong Liauw <hanloongliauw@gmail.com>
2015-04-26 13:00:43 +1000

    added float action component

--- app/components/float-action/component.js
+++ app/components/float-action/component.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.View.extend({
+export default Ember.Component.extend({
   didInsertElement: function() {
     Ember.$('.tooltipped').tooltip({delay: 50});
   }
