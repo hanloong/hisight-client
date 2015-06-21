38f60db8748977555868c266da8c2b20f68d1364
Han Loong Liauw <hanloongliauw@gmail.com>
2015-06-21 22:00:11 +1000

    removed unused controller

--- app/pods/project/factors/route.js
+++ app/pods/project/factors/route.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    newFactor: function(area) {
+      return this.store.createRecord('factor', {area: area});
+    }
+  }
 });
