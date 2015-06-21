6d03515245e146e95f8eb8d9b887a20918227b94
Han Loong Liauw <hanloongliauw@gmail.com>
2015-06-21 23:32:24 +1000

    factors with many actions

--- app/models/area.js
+++ app/models/area.js
@@ -3,7 +3,11 @@ import DS from 'ember-data';
 var Area = DS.Model.extend({
   name:         DS.attr('string'),
   factors:      DS.hasMany('factor', {async: true}),
-  project:      DS.belongsTo('project')
+  project:      DS.belongsTo('project'),
+
+  nameInputId: function() {
+    return 'area_name_' + this.get('id');
+  }.property('id'),
 });
 
 export default Area;
--- app/pods/project/factors/route.js
+++ app/pods/project/factors/route.js
@@ -4,6 +4,28 @@ export default Ember.Route.extend({
   actions: {
     newFactor: function(area) {
       return this.store.createRecord('factor', {area: area});
-    }
+    },
+    updateFactor: function(factor) {
+      factor.save().then((model) => {
+      }).catch((errors) => {
+        console.log(errors);
+      });
+    },
+    destroyFactor: function(factor) {
+      factor.destroyRecord();
+    },
+    newArea: function(project) {
+      return this.store.createRecord('area', {project: project});
+    },
+    updateArea: function(area) {
+      area.save().then((model) => {
+        console.log('area update');
+      }).catch((errors) => {
+        console.log(errors);
+      });
+    },
+    destroyArea: function(area) {
+      area.destroyRecord();
+    },
   }
 });
