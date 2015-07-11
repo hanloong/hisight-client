46931ae173d9e034cd1c2c0f2881707eaf697ea1
Han Loong Liauw <hanloongliauw@gmail.com>
2015-07-11 21:39:06 +1000

    fixed side bar

--- app/pods/components/top-nav/component.js
+++ app/pods/components/top-nav/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement: function() {
-    this.$(".sidebar-toggle").sideNav({closeOnClick: true});
+    this.$(".sidebar-toggle").sideNav({closeOnClick: false});
     this.$('.modal-trigger').leanModal();
   }
 });
