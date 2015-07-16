c6e6c12d78d85d0238321a1285f7f666e215672d
Han Loong Liauw <hanloongliauw@gmail.com>
2015-07-16 10:15:36 +1000

    WIP

--- config/environment.js
+++ config/environment.js
@@ -24,12 +24,14 @@ module.exports = function(environment) {
     ENV['simple-auth-oauth2'] = {
       serverTokenEndpoint: 'http://localhost:3000/oauth/token'
     }
+    ENV.apiHost = 'http://localhost:3000/';
   }
 
   if (environment === 'test') {
     // Testem prefers this...
     ENV.baseURL = '/';
     ENV.locationType = 'none';
+    ENV.apiHost = null;
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
@@ -39,7 +41,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.apiHost = 'https://api.hisight.io/';
   }
 
   ENV['simple-auth'] = {
@@ -52,7 +54,7 @@ module.exports = function(environment) {
     'style-src': "'self' 'unsafe-inline'",
     'font-src': "*",
     'report-uri': "'self'",
-    'connect-src': ENV.apiURL
+    'connect-src': ENV.apiHost
   };
 
   return ENV;
